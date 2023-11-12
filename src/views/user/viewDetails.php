96ec3394e0597cc577d1a25fb4d9e32e37996480
Jean Hubert <izoret.raphael@gmail.com>
2023-11-12 15:10:51 +0100

    ah ben fini co en fait

--- src/views/user/viewDetails.php
+++ src/views/user/viewDetails.php
@@ -1,6 +1,7 @@
 <?php
 if (isset($user))
-    echo '<p>Current user login : ' . $user->login . '</p>
-        <a href="?controller=Connection&action=logOut">Log out</a>';
+    echo '<p>Current user login : ' . $user->login . '</p>';
 else
     echo '<p>No one is connected !</p>';
+?>
+<a href="?controller=Connection&action=logOut">Log out</a>
